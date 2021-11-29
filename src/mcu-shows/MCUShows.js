8a094cf86f0d83587a1053e05953af0cfc2fb1cf
Ruth Gough <ruthegough5@gmail.com>
2021-11-29 10:49:35 -0600

    Final stages of exercise and bonus. Corrections and updates to Bonus added.

--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -1,24 +1,27 @@
-// 1a Import the MCUShows.css file
-import './MCUShows.css'
+// FINAL REACT EXERCISE
 
-// 1b function component
-const MCUShows = ({ dates, randomIndex }) => {
-  // 6f
-  const getAvenger = () => randomIndex();
+// 1a
+import './MCUShows.css';
+
+// 1b & 5c & BONUS 6f & 7d
+function MCUShows({dates, indexTwo, numTwo}){
   // 1c
-  return (
+  return(
     // 4a
-    <div className='MCUShows'>
-      {/* 3a Set font and Underline to h1*/}
+    <div className="MCUShows">
+      {/* 3a */}
       <h1 style={{fontFamily: 'Georgia', textDecoration: 'underline'}}>Disney+</h1>
-      <h2>Upcoming MCU Show Release Dates</h2>
+      <h2>MCU Show Release Dates</h2>
+      {/* 5c */}
       <p>WandaVision: {dates.wandaVision}</p>
       <p>The Falcon and the Winter Soldier: {dates.falconWinter}</p>
       <p>Loki: {dates.loki}</p>
-      <p>What If: {dates.whatIf}</p>
+      <p>What If...?: {dates.whatIf}</p>
       <p>Hawkeye: {dates.hawkeye}</p>
-      {/* 6e */}
-      <button onClick={getAvenger}>RANDOM MCU CHARACTER</button>
+      {/* BONUS 6e & 6f */}
+      <button onClick={indexTwo}>RANDOM MCU CHARACTER</button>
+      {/* BONUS 7c & 7d */}
+      <button onClick={numTwo}>NEXT MCU CHARACTER</button>
     </div>
   );
 }
